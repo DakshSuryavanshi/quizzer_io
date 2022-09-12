32b1142c2adc275e9af4d366c71d4610a8bd884b
DakshSuryavanshi <dakshsuryavanshi2003@gmail.com>
2022-09-12 21:08:50 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/quizerio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quizerio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ////this (<>) here signifies the type of list
+    var questions = <String>[
+      "What's you favourite colour?",
+      "What's you favourite animal?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Quizer.io"),
+        ),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            ElevatedButton(
+              child: Text("Answer 1"),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text("Answer 2"),
+              onPressed: () => print("Answer 2 chosen"),
+            ),
+            ElevatedButton(
+              child: Text("Answer 3"),
+              onPressed: () {
+                print("Answer 3 chosen");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
